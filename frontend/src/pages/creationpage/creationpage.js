bde7ff16c3ca17d237129d6fe8fad1c44928655e
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-02 21:26:02 +0530

    Quiz Creation modified

--- frontend/src/pages/creationpage/creationpage.js
+++ frontend/src/pages/creationpage/creationpage.js
@@ -13,6 +13,12 @@ const CreationPage =()=>{
     const [quizheading, setQuizheading] = useState();
     const [quizdescription, setQuizdescription] = useState();
     const [newquestion, setNewquestion] = useState();
+    const [question, setQuestion] = useState();
+    const [questionimage, setQuestionimage] = useState();
+    const [correctanswer, setCorrestanswer] = useState();
+    const [wronganswer1, setWronganswer1] = useState();
+    const [wronganswer2, setWronganswer2] = useState();
+    const [wronganswer3, setWronganswer3] = useState();
 
     useEffect(()=>{
         setTeacher(getCache('HDLGenHub_Teacher'));
@@ -35,13 +41,24 @@ const CreationPage =()=>{
         }
     }
 
+    const handleAddquestion=()=>{
+        alert(quizheading);
+        alert(quizdescription);
+        alert(question);
+        alert(questionimage);
+        alert(correctanswer);
+        alert(wronganswer1);
+        alert(wronganswer2);
+        alert(wronganswer3);
+    }
+
     return(
         <div className='creationpagecontainer'>
             <div className='creationcoursecontainer'>
                 <h1>Course Creation</h1>
                 <div className='creationcourseform'>
                     <label>Course Heading</label>
-                    <input alt='name' value={name} onChange={(e)=>setName(e.target.value)} type='text'></input>
+                    <input alt='name' value={name} onChange={(e)=>setQuizheading(e.target.value)} type='text'></input>
                     <label>Cover Image Url</label>
                     <input alt='coverimage' value={coverimage} onChange={(e)=>setCoverimage(e.target.value)} type='text'></input>
                     <label >Course Description</label>
@@ -62,8 +79,20 @@ const CreationPage =()=>{
                         </button>
                     } modal nested>{
                         <div className='quizcreation-popup'>
-                            <div>
-                                
+                            <div className='quizcreation-popup-form'>
+                                <label>Question</label>
+                                <input onChange={(e)=>setQuestion(e.target.value)} type='text'></input>
+                                <label>Question Image</label>
+                                <input onChange={(e)=>setQuestionimage(e.target.value)} type='text'></input>
+                                <label>Correct Answer</label>
+                                <input onChange={(e)=>setCorrestanswer(e.target.value)} type='text'></input>
+                                <label>Wrong Answer 1</label>
+                                <input onChange={(e)=>setWronganswer1(e.target.value)} type='text'></input>
+                                <label>Wrong Answe 2r</label>
+                                <input onChange={(e)=>setWronganswer2(e.target.value)} type='text'></input>
+                                <label>Wrong Answer 3</label>
+                                <input onChange={(e)=>setWronganswer3(e.target.value)} type='text'></input>
+                                <button onClick={handleAddquestion}>Add</button>
                             </div>
                         </div>
                     }</Popup>        
