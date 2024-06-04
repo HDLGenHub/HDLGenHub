5dcdecb51cb0bca7974e13195122db3ad3b015fb
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-04 20:56:09 +0530

    Codespace designed

--- frontend/src/compiler/components/coding.js
+++ frontend/src/compiler/components/coding.js
@@ -64,12 +64,16 @@ const Coding=()=>{
         alert("File Saved");
     }
 
+    const handleSelectfile=(file)=>{
+        setCode(file.code);
+    }
+
     return(
         <div className='codespace-container'>
             <div className='codespace-files'>
                 {codefiles?codefiles.data.map((codefile)=>(
                     <div className='codespace-file-each'>
-                        <p>{codefile.filename}</p>
+                        <button onClick={()=>handleSelectfile(codefile)}>{codefile.filename}</button>
                     </div>
                 )):null}
             </div>
