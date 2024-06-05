ecceccf835a07e3f1eac9cf9eba0e90f55cd2737
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-05 14:48:38 +0530

    Roughly Designed Quiz page

--- frontend/src/forstudent/components/questioncard/questioncard.js
+++ frontend/src/forstudent/components/questioncard/questioncard.js
@@ -1,3 +1,4 @@
+import Coverimage from '../coverimage/coverimage';
 import './questioncard.css';
 
 const Questioncard=(questionData)=>{
@@ -6,12 +7,38 @@ const Questioncard=(questionData)=>{
     return(
         <div className='questioncard-conatiner'>
             <div className='questioncard-question'>
-                <p>{JSON.stringify(questiondata)}</p>
                 <p>{questiondata.problem}</p>
-                <img src={questiondata.image}/>
-                <p>Choose the correct answer</p>
+                {questiondata.image?(<Coverimage Image={questiondata.image}/>):null}
             </div>
             <div className='questioncard-answers'>
+                {(questiondata.answer && questiondata.wronganswer1 && questiondata.wronganswer2 && questiondata.wronganswer3)?(
+                    <div className='questioncard-answers-set'>
+                        <p>Choose the correct answer</p>
+                        <div className='questioncard-answers-each'>
+                            <label>
+                                <input type='radio'></input>
+                                {questiondata.answer}
+                            </label>
+                            <label>
+                                <input type='radio'></input>
+                                {questiondata.wronganswer1}
+                            </label>
+                            <label>
+                                <input type='radio'></input>
+                                {questiondata.wronganswer2}
+                            </label>
+                            <label>
+                                <input type='radio'></input>
+                                {questiondata.wronganswer3}
+                            </label>
+                        </div>
+                    </div>
+                ):(
+                    <div className='questioncard-answer-single'>
+                        <p>Enter correct answer</p>
+                        <input></input>
+                    </div>
+                )}
             </div>
         </div>
     );
