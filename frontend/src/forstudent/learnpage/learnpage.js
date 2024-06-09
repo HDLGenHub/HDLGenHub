51491240a15c69cfffc5805723dea5431a4a5770
nadeeshani00jay@gmail.com <139541836+NadeeshaniJay@users.noreply.github.com>
2024-06-10 02:30:06 +0530

    Modify Learn page and enrolled course card

--- frontend/src/forstudent/learnpage/learnpage.js
+++ frontend/src/forstudent/learnpage/learnpage.js
@@ -4,13 +4,12 @@ import Enrolledcourses from "../components/enrolledcourses/enrolledcourses";
 const LearnPage = () => {
     return (
         <div class="m-20">
-            
         <div>
-            <h2>Enrolled Courses</h2>
+            <h2 class="text-2xl ml-5">Enrolled Courses</h2>
             <Enrolledcourses />
         </div>
-        <hr style={{ height: "2px", backgroundColor: "black" }}></hr>
-        <div>
+        <hr/>
+        <div class="text-2xl ml-5 mt-10">
             <h2>All Courses</h2>
             <Courses />
         </div>
