c20b8df6eaac188236c64bc7163e82486569d87e
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-25 15:56:45 +0530

    cors add everything

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+app.use(cors(['*']));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, {
