7f711a20e648cc8bcb346fa813b018f2798fc1a4
nadeeshani00jay@gmail.com <139541836+NadeeshaniJay@users.noreply.github.com>
2024-06-26 11:28:08 +0530

    Document Adding Done

--- frontend/src/pages/creationpage/creationpage.js
+++ frontend/src/pages/creationpage/creationpage.js
@@ -175,7 +175,7 @@ const CreationPage =()=>{
             </div>
 
             <div className='challenges-creation-container'>
-                <h1>Course Creation</h1>
+                <h1>Challenge Creation</h1>
                 <div className='creationchallengesform'>
                     <label>Challenge Heading</label>
                     <input alt='name' value={name} onChange={(e)=>setQuizheading(e.target.value)} type='text'></input>
@@ -183,14 +183,10 @@ const CreationPage =()=>{
                     <input alt='description' value={description} onChange={(e)=>setDescription(e.target.value)} className='creationchallengeform-description' type='text'></input>
                     <label>Is Challenge Private</label>
                     <div className='challenges-radio'>
-                        <div>
                             <label>Yes</label>
                             <input name='bool' type='radio'/>
-                        </div>
-                        <div>
                             <label>No</label>
                             <input name='bool' type='radio'/>
-                        </div>
                     </div>
                     <button onClick={postCourse}>Create Course</button>
                 </div>
